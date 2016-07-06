97508f86385357e09b1f4cf0051de6caa10a2340
dsharp <dsshar@gmail.com>
2016-07-06 18:01:37 -0400

    Code Doddles

---
+++ statistics.py
@@ -0,0 +1,74 @@
+import math
+import random
+import matplotlib.pyplot as plt
+from collections import Counter
+from linear_algebra import sum_of_squares, dot, shape, get_column, get_row, make_matrix
+
+def mean(x):
+    return sum(x) / len(x)
+
+
+def median(x):
+    """finds the 'middle-most' value of v"""
+    n = len(x)
+    sorted_x = sorted(x)
+    midpoint = n // 2
+
+    if n % 2 == 1:
+        return sorted_x[midpoint]
+    else:
+        lo = midpoint - 1
+        hi = midpoint
+        return (sorted_x[lo] + sorted_x[hi]) / 2
+
+
+def quantile(x, p):
+    """returns the pth-percentile value in x"""
+    p_index = int(p * len(x))
+    return sorted(p)[p_index]
+
+
+def mode(x):
+    counts = Counter(x)
+    max_count = max(counts.values())
+    return [x_i for x_i, count in counts.interitems() if count == max_count]
+
+
+def data_range(x):
+    return max(x) - min(x)
+
+
+def interquartile_range(x):
+    return quantile(x, 0.75) - quantile(x, 0.25)
+
+
+def de_mean(x):
+    """translate x by subtracting its mean (so the result has mean 0"""
+    x_bar = mean(x)
+    return [x_i - x_bar for x_i in x]
+
+
+def variance(x):
+    """assume x has at least two elements"""
+    n = len(x)
+    deviations = de_mean(x)
+    return sum_of_squares(deviations) / (n - 1)
+
+
+def standard_deviation(x):
+    return math.sqrt(variance(x))
+
+
+def covariance(x, y):
+    n = len(x)
+    return dot(de_mean(x), de_mean(y)) / (n - 1)
+
+
+def correlation(x, y):
+    stddev_x = standard_deviation(x)
+    stddev_y = standard_deviation(y)
+    if stddev_y > 0 and stddev_x > 0:
+        return covariance(x, y) / stddev_x / stddev_y
+    else:
+        return 0  # if no variation, correlation is zero
+
--- working_with_data.py
+++ working_with_data.py
@@ -1,22 +1,30 @@
 import math
 import random
+import csv
+import dateutil.parser
 import matplotlib.pyplot as plt
 from collections import Counter
 from normal_distribution import inverse_normal_cdf
+from linear_algebra import shape, get_column, make_matrix
+from statistics import correlation
+
 
 def bucketsize(point, bucket_size):
     """floor the point to the next lower multiple of bucket size"""
     return bucket_size * math.floor(point / bucket_size)
 
+
 def make_histogram(points, bucket_size):
     """buckets the points and counts how many in each bucket"""
     return Counter(bucketsize(point, bucket_size) for point in points)
+
 
 def plot_histogram(points, bucket_size, title=""):
     histogram = make_histogram(points, bucket_size)
     plt.bar(histogram.keys(), histogram.values(), width=bucket_size)
     plt.title()
     plt.show()
+
 
 def plot_uni_nor():
     random.seed(0)
@@ -31,9 +39,11 @@ def plot_uni_nor():
 
     plot_histogram(normal, 10, "Normal Histogram")
 
+
 def random_normal():
     """returns a random draw from a standard normal distribution"""
     return inverse_normal_cdf(random.random())
+
 
 xs = [random_normal() for _ in range(1000)]
 ys1 = [x + random_normal() / 2 for x in xs]
@@ -48,4 +58,107 @@ plt.title("Very different joint distribution")
 plt.show()
 
 print("correlation of xs and ys1 {} and correlation of xs and ys2 {}".format(correlation(xs, ys1),
-                                                                             correlation()))
+                                                                             correlation(xs, ys2)))
+
+
+def correlation_matrix(data):
+    """returns the num_columns x num_columns matrix whose (i,j)th entry
+    is the correlation between columns i and j of the data"""
+
+    _, num_columns = shape(data)
+
+    def matrix_entry(i, j):
+        return correlation(get_column(data, i), get_column(data, j))
+
+    return make_matrix(num_columns, num_columns, matrix_entry)
+
+
+def visual_approach():
+    num_points = 100
+
+    def random_row():
+        row = [None, None, None, None]
+        row[0] = random_normal()
+        row[1] = -5 * row[0] + random_normal()
+        row[2] = row[0] + row[1] + 5 * random_normal()
+        row[3] = 6 if row[2] > -2 else 0
+        return row
+
+    random.seed(0)
+    data = [random_row()
+            for _ in range(num_points)]
+    _, num_columns = shape(data)
+    fig, ax = plt.subplots(num_columns, num_columns)
+
+    for i in range(num_columns):
+        for j in range(num_columns):
+
+            # Scatter columm_j on the x-axis vs column_i on the y-axis
+            if i != j:
+                ax[i][j].scatter(get_column(data, j), get_column(data, i))
+
+            # unlesss i == j, in which case show the series name
+            else:
+                ax[i][j].annotate("series {}".format(i), (0.5, 0.5),
+                                  xycoords='axes fraction',
+                                  ha="center", va="center")
+
+            # then hide dxis labels except left and bottom charts
+            if i < num_columns - 1: ax[i][j].xaxis.set_visible(False)
+            if j > 0: ax[i][j].yaxis.set_visible(False)
+
+    # fix the bottom right and top left except left and bottom charts
+    ax[-1][-1].set_xlim(ax[0][-1].get_xlim())
+    ax[1][1].set_xlim(ax[0][1].get_ylim())
+
+    plt.show()
+
+# visual_approach()
+
+def parse_row(input_row, parsers):
+    """given a list of parsers (some of which may be None)
+    apply the appropriate one to each element of the input_row"""
+    return [try_or_none(parser)(value) if parser is not None else value
+            for value, parser in zip(input_row, parsers)]
+
+
+def parse_rows_with(reader, parsers):
+    """wrap a reader to apply the parers to each of its rows"""
+    for row in reader:
+        yield parse_row(row, parsers)
+
+
+def try_or_none(f):
+    """Wrap f to return None if f raises an exception
+    assumes f takes only one input"""
+    def f_or_none(x):
+        try: return(x)
+        except: return None
+
+    return f_or_none
+
+
+def try_parse_field(field_name, value, parser_dict):
+    """try to parse value using the appropriate function from parser_dic"""
+    parser = parser_dict.get(field_name) #None if no such entry
+    if parser is not None:
+        return try_or_none(parser)(value)
+    else:
+        return value
+
+
+def parse_dict(input_dict, parser_dict):
+    return { field_name : try_parse_field(field_name, value, parser_dict)
+             for field_name, value in input_dict.iteritems() }
+
+if __name__ == "__main__" :
+    data = []
+    with open("comma_delimited_stock_prices.csv", "r") as f:
+        reader = csv.reader(f)
+        for line in parse_rows_with(reader, [dateutil.parser.parse, None, float]):
+            data.append(line)
+
+    for row in data:
+        if any(x is None for x in row):
+            print(row)
+    print(data)
